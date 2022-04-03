655fa0e299ede9faf6dc472e102c42f236e0f4a3
avivk9 <avivkeinan1@gmail.com>
2022-04-03 10:19:00 +0300

    first

---
+++ source/main.py
@@ -0,0 +1,22 @@
+from getpass import getpass
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host="localhost",
+        user=input("Enter username: "),
+        password=input("Enter password: "),
+    ) as connection:
+        print(connection)
+        ### CREATE NEW DB
+        # create_db_query = "CREATE DATABASE online_movie_rating"
+        # with connection.cursor() as cursor:
+        #     cursor.execute(create_db_query)
+
+        ### WATCH ALL EXISTING DBS:
+        with connection.cursor() as cursor:
+            cursor.execute("SHOW DATABASES")
+            for db in cursor:
+                print(db)
+except Error as e:
+    print(e)
